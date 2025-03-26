241f830f069538280dab3532c35d41105518292a
YeahmanS <yamansingh85@gmail.com>
2025-03-27 02:40:19 +0530

    finished timer project but having some issues need to fix them too

--- Projects/timer/script.js
+++ Projects/timer/script.js
@@ -2,19 +2,54 @@ const input = document.getElementById("input")
 const start_button = document.getElementById("start")
 const timer_text = document.getElementById("timer-text")
 
+
 function alertkaro(){
     let innertext = parseInt(input.value)
     console.log(innertext)
     if(isNaN(innertext)){
-        input.style.borderColor = "red"
         timer_text.innerText = "Please Enter Integer"
         timer_text.style.color = "red"
+        start_button.disabled = true;
 
-    } else {
+
+    } else if (innertext < 0){
+        timer_text.style.color="red"
+        timer_text.innerText = "Please Enter Positive Integer"
+        start_button.disabled = true;
+    }
+    else {
         timer_text.style.color=""
-        input.style.borderColor=""
         timer_text.innerText = ""
+        start_button.disabled = false;
+    }
+
+
+    if (intervalid !=null){
+        clearInterval(intervalid)
+        intervalid=null
+        timer_text.innerText = "Time's Up"
+        start_button.disabled = false
     }
 }
 
+function starttimer(){
+    let user_input = parseInt(input.value)
+
+    let intervalid=setInterval(function (){
+        if (user_input < 0){
+            clearInterval(intervalid)
+            intervalid=null
+            timer_text.innerText = "Time's Up"
+            start_button.disabled = false
+        } else { 
+            timer_text.innerText=`Time Remaining : ${user_input} sec`;
+        }  
+        
+        user_input=user_input-1 ;
+},1000)
+}
+
 input.addEventListener("input",alertkaro);
+
+start_button.addEventListener("click",()=>(start_button.disabled = true))
+start_button.addEventListener("click",starttimer)
