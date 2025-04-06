fa0b12170e3a2773335981718b00e9eaa3ca9218
YeahmanS <yamansingh85@gmail.com>
2025-04-06 23:15:27 +0530

    sharpened my api callings skills sort of and made good looking random quote generator

---
+++ Week 9/api-calling/script.js
@@ -0,0 +1,17 @@
+const new_quote_button = document.getElementById("getQuote")
+const text_area = document.getElementById("Quote")
+
+async function getdata(){
+    const url = 'https://api.freeapi.app/api/v1/public/quotes/quote/random';
+    const result = await fetch(url)
+
+    return await result.json()
+}
+
+async function updateQuote(){
+    const quote = await getdata()
+
+    text_area.innerText = quote.data.content
+}
+
+new_quote_button.addEventListener("click",updateQuote)
